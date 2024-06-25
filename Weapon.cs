b89168b0259801eb61e7efa4e9a67d98ecee489e
Eugene Anufrienko <66831252+anufrienkoek@users.noreply.github.com>
2024-06-25 15:49:42 +0300

    Update Weapon.cs

--- Weapon.cs
+++ Weapon.cs
@@ -3,57 +3,78 @@ namespace NapilnicTask1
     public abstract class Weapon
     {
         private readonly Clip _clip;
-        private readonly Player _player;
 
-        protected Weapon(Clip clip, Player player)
+        protected Weapon(Clip clip) 
         {
-            _clip = clip;
-            _player = player;
+            _clip = clip ?? throw new ArgumentNullException(nameof(clip));
         }
 
         public void Fire(Player player)
         {
+            if (player == null) throw new ArgumentNullException(nameof(player));
             if (_clip.CurrentBulletsCount <= 0) return;
             
             _clip.RemoveBullet();
-            _player.TakeDamage(_clip.Bullet.Damage);
+            player.TakeDamage(_clip.Bullet.Damage);
         }
     }
 
     public abstract class Clip
     {
-        public Bullet Bullet {get; private set;}
-        public uint CurrentBulletsCount {get; private set; } 
-        public uint MaxBulletsCount {get; private set; }
+        private readonly uint _maxBulletsCount;
+        private uint _currentBulletsCount;
 
         protected Clip(Bullet bullet, uint maxBulletsCount, uint currentBulletsCount)
         {
-            Bullet = bullet; 
-            MaxBulletsCount = maxBulletsCount;
-            CurrentBulletsCount = currentBulletsCount;
+            Bullet = bullet ?? throw new ArgumentNullException(nameof(bullet));
+            if (maxBulletsCount <= 0) throw new ArgumentOutOfRangeException(nameof(maxBulletsCount));
+            if (currentBulletsCount > maxBulletsCount) throw new ArgumentOutOfRangeException(nameof(currentBulletsCount));
+
+            _maxBulletsCount = maxBulletsCount;
+            _currentBulletsCount = currentBulletsCount;
         }
+
+        public Bullet { get; }
 
         public void RemoveBullet()
         {
-            if(CurrentBulletsCount > 0) 
-                CurrentBulletsCount--;
+            if(_currentBulletsCount > 0) 
+                _currentBulletsCount--;
         }
     }
 
     public abstract class Bullet
     {
-        public uint Damage {get; private set; }
+        protected Bullet (uint damage)
+        {
+            if (damage <= 0) throw new ArgumentOutOfRangeException(nameof(damage));
+        
+            Damage = damage;
+        }
+
+        public uint Damage {get; }
     }
 
 
     public abstract class Player
     {
-        public bool IsDead {get; private set; }
         private uint _health;
+
+        protected Player(uint health)
+        {
+            if (health <= 0) throw new ArgumentOutOfRangeException(nameof(health));
+
+            _health = health;
+            IsDead = false;
+        }
+
+        public bool IsDead {get; private set; }
 
         public void TakeDamage(uint damage)
         {
-            if (damage <= _health)
+            if (damage <= 0) throw new ArgumentOutOfRangeException(nameof(damage));
+
+            if (damage < _health)
             {
                 _health -= damage;
             }
@@ -72,13 +93,17 @@ namespace NapilnicTask1
 
     public class Bot
     {
-        private Weapon Weapon {get; set; }
+        private Weapon _weapon;
 
         public Bot(Weapon weapon)
         {
-            Weapon = weapon;
+            _weapon = weapon;
         }
 
-        public void OnSeePlayer(Player player) => Weapon.Fire(player);
+        public void OnSeePlayer(Player player) 
+        {
+            if (player == null) throw new ArgumentNullException(nameof(player));
+            _weapon.Fire(player);
+        }
     }
 }
